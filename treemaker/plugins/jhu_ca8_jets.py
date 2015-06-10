647b969f4f68e2d5c547ea0bdeae82366d858422
Ben Rosser <rosser.bjr@gmail.com>
2015-06-10 17:27:12 -0400

    Added tau32 and tau21 to the treemaker.

--- treemaker/plugins/jhu_ca8_jets.py
+++ treemaker/plugins/jhu_ca8_jets.py
@@ -1,7 +1,9 @@
 import array
+import math
 
 # Variant of the jets plugin shipped by Treemaker as an example.
 # Keeps the first three jhuca8 jets.
+# Any per-jet variable goes in here.
 
 # TODO: turn this back into a knob somewhere.
 numJets = 3
@@ -16,18 +18,34 @@ class Jet:
 
 		self.initVars()
 
+	def __matchVectors(vectors, vector):
+		"""	A modified version of MatchCol from JetTools.py, written by Marc."""
+		value = -1
+		deltaR = 0.4
+		for i in range(len(vectors)):
+			matching = ROOT.TLorentzVector()
+			matching.SetPtEtaPhiM(matching[i].Pt(), matching[i].Eta(), matching[i].Phi(), matching[i].Mass())
+			deltaR = math.abs(vector.DeltaR(matching))
+			if deltaR < 0.4:
+				value = i
+		if deltaR > 0.4:
+			value = -1
+		return value
+
 	def setup(self, variables):
-		"""Initializes branch arrays for all jet variables. Returns a copy of the
+		"""	Initializes branch arrays for all jet variables. Returns a copy of the
 		variables dictionary."""
 		variables[self.jetName + 'pt'] = array.array('f', [-1.0])
 		variables[self.jetName + 'mass'] = array.array('f', [-1.0])
 		variables[self.jetName + 'eta'] = array.array('f', [100.0])
 		variables[self.jetName + 'phi'] = array.array('f', [100.0])
 		variables[self.jetName + 'csv'] = array.array('f', [0.0])
+		variables[self.jetName + 'tau21'] = array.array('f', [1.0])
+		variables[self.jetName + 'tau32'] = array.array('f', [1.0])
 		return variables
 
 	def analyze(self, variables, labels):
-		"""Runs the treemaking step. Labels is a dictionary containing already-
+		"""	Runs the treemaking step. Labels is a dictionary containing already-
 		loaded handles."""
 		# Do the analysis step.
 		jetCollection = 'PrunedCA8'
@@ -43,6 +61,25 @@ class Jet:
 			self.phi = fourVector[self.number - 1].Phi()
 			self.pt = fourVector[self.number - 1].Pt()
 			self.csv = csvVector[self.number - 1]
+			
+			# Match the jet to the unpruned jet.
+			jetVector = ROOT.TLorentzVector()
+			jetVector.SetPtEtaPhiM(self.pt, self.eta, self.phi, self.mass)
+			unprunedVectors = labels['jhuCa8']['UnprunedCA8'].product()
+			unpruned = self.__matchVectors(unprunedVectors, jetVector)
+			
+			tau1 = labels['jhuCa8']['UnprunedCA8tau1'].product()
+			tau2 = labels['jhuCa8']['UnprunedCA8tau1'].product()
+			tau3 = labels['jhuCa8']['UnprunedCA8tau1'].product()
+			if tau1[unpruned] == 0:
+				self.tau21 = 100
+			elif tau1[unpruned] > 0:
+				self.tau21 = tau2[unpruned] / tau1[unpruned]
+			if tau2[unpruned] == 0:
+				self.tau32 = 100
+			elif tau2[unpruned] > 0:
+				self.tau32 = tau3[unpruned] / tau2[unpruned]
+			
 		except:
 			# If this fails, set all variables to defaults.
 			self.initVars()
@@ -51,19 +88,21 @@ class Jet:
 		return variables
 
 	def reset(self, variables):
-		"""Resets the jet variables and returns an updated copy of the variable
+		"""	Resets the jet variables and returns an updated copy of the variable
 		array dictionary."""
 		self.initVars()
 		return self.update(variables)
 
 	def update(self, variables):
-		"""Updates the actual arrays, returns an updated copy of the variable
+		"""	Updates the actual arrays, returns an updated copy of the variable
 		dictionary."""
 		variables[self.jetName + 'mass'][0] = self.mass
 		variables[self.jetName + 'eta'][0] = self.eta
 		variables[self.jetName + 'phi'][0] = self.phi
 		variables[self.jetName + 'pt'][0] = self.pt
 		variables[self.jetName + 'csv'][0] = self.csv
+		variables[self.jetName + 'tau21'][0] = self.tau21
+		variables[self.jetName + 'tau32'][0] = self.tau32
 		return variables
 
 	def initVars(self):
