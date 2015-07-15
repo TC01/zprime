311af90519bd9286ea9be342e15612977f774aac
Ben Rosser <bjr@fnal.gov>
2015-07-15 18:15:15 -0500

    Fixed bug in template morpher.

--- fitter/TT_TemplateMorpher.py
+++ fitter/TT_TemplateMorpher.py
@@ -9,8 +9,7 @@ model = build_ttfit()
 for p in model.distribution.get_parameters():
     model.distribution.set_distribution_parameters(p, range = [-5.0, 5.0]) # limits how far we can go with any variation on a parameter, important because if the subtracted ttbar estimate gets too small we can get negative expected events and this will crash or blow up.
 signal_process_groups = {'': []} # just returns nuisance params (no signal)
-parVals = mle(model, input = 'data', n=1, signal_process_groups = signal_process
-_groups)
+parVals = mle(model, input = 'data', n=1, signal_process_groups = signal_process_groups)
 print parVals
 
 model_summary(model)
